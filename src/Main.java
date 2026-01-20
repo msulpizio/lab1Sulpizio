f43ba235ea2eabb31da285375354613bb078be23
matthewsulpizio <mjs9927@psu.edu>
2026-01-20 11:45:59 -0500

    added color options

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        Motorcycle m1 = new Motorcycle();
+        Motorcycle m1 = new Motorcycle("RED");
+        System.out.println(m1.getColor());
+
+        Motorcycle m2 = new Motorcycle("Blue");
+        System.out.println(m2.getColor());
     }
 }
